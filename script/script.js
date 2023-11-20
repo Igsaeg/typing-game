8f30801bbd4e1cef679aebbafa346a8a87f0ddc6
Igsaeg <60771249+Igsaeg@users.noreply.github.com>
2023-11-20 14:59:16 +0800

    preview

--- script/script.js
+++ script/script.js
@@ -1,6 +1,5 @@
 const scoreElement = document.getElementById("score");
 const wordElement = document.getElementById("words");
-const feedbackElement = document.getElementById("feedback");
 const inputElement = document.getElementById("input");
 let wordValue = '';
 let letterArray = [];
@@ -10,21 +9,24 @@ let place = 0;
 
 function initGame() {
     place = 0;
-    wordValue = words[Math.floor(Math.random() * words.length)];
+    wordValue = sentences[Math.floor(Math.random() * sentences.length)];
     letterArray = wordValue.split('');
-    wordElement.innerText = wordValue.toUpperCase();
+    wordElement.innerText = wordValue;
 }
 function checkInput() {
     if (userInput == letterArray[place]) {
         place = (place + 1 !== letterArray.length) ? place + 1 : (initGame(), 0);
         let highlighted = letterArray.map((letter, i) => `<span style="${i < place ? 'color: green;' : ''}">${letter}</span>`).join('');
         score += 100;
-        wordElement.innerHTML = highlighted.toUpperCase();
-        feedbackElement.innerHTML = 'Correct!';
+        wordElement.innerHTML = highlighted;
         new Audio('assets/correct.mp3').play();
     } else {
+        let highlighted = letterArray.map((letter, i) => {
+            if (letter === ' ') { return '_<wbr>'; }
+            else { return `<span style="${i < place ? 'color: green;' : (i == place ? 'color: red;' : '')}">${letter}</span>`; }
+        }).join('');
         score = Math.max(0, score - 50);
-        feedbackElement.innerHTML = `Wrong, you pressed ${userInput.toUpperCase()}.`;
+        wordElement.innerHTML = highlighted;
         new Audio('assets/wrong.mp3').play();
     }
     userInput = '';
@@ -39,15 +41,15 @@ function mobileMode() {
     document.getElementById('button').classList.toggle('active');
 }
 document.addEventListener('keydown', (event) => {
-    if (letterRegex.test(event.key) && !userInput) {
-        userInput = event.key.toLowerCase();
+    if (letterRegex.test(event.key) && !userInput && !specialKeys.includes(event.key)) {
+        userInput = event.key;
         checkInput();
     }
 });
 document.addEventListener('keyup', () => { userInput = ''; });
 inputElement.addEventListener('input', () => {
     if (letterRegex.test(inputElement.value)) {
-        userInput = inputElement.value.toLowerCase();
+        userInput = inputElement.value;
         checkInput();
     }
     inputElement.value = '';
